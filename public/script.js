a2db7063a8c0d595db07990dd77eccbbc49bb97b
undefined8 <dzamilatskiy2019@bklawtech.com>
2017-11-06 23:18:18 +0000

    added data to webpage

--- public/script.js
+++ public/script.js
@@ -1,8 +1,31 @@
-//Unit 1 Portfolio with Firebase
-// Use Firebase to store the images links and info for your projects. 
-// Use Jquery to dynamically display the information from firebse. 
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCAIhrUeRq5w64TIk2Bf_uG3OanUE-mlzA",
+    authDomain: "portfolio-3ac96.firebaseapp.com",
+    databaseURL: "https://portfolio-3ac96.firebaseio.com",
+    projectId: "portfolio-3ac96",
+    storageBucket: "portfolio-3ac96.appspot.com",
+    messagingSenderId: "1066703533063"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+var projects_ref = database.ref('/projects');
 
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
+projects_ref.once('value', function(snapshot) {
+    var projects = snapshot.val();
+    console.log(projects);
 
+    for (var i in projects) {
+        var div = $("<div class='col-md-3'>");
+        var title = $('<p>');
+        var anchor = $("<a href=" + projects[i].site_url + ">");
+        var img = $("<img class='img-fluid' src=" + projects[i].image_url +">");
+        title.text(projects[i].title);
+        div.append(title);
+        anchor.append(img);
+        div.append(anchor);
+        $("#proj").append(div);
+    }
+});
+
+var projects = snapshot.val();
